ab2516029db2b0671b1097f5d19cc321c9f76554
lildikie <127436775+lildikie@users.noreply.github.com>
2023-03-22 21:14:14 -0400

    Add files via upload

---
+++ lab06.py
@@ -0,0 +1,22 @@
+def string_to_data(data_string):
+    ReturnData = ""
+
+    for index in data_string:
+        if i < 7:
+            ReturnData = ReturnData + str(i) + 3
+        else:
+            if i == 7:
+                i = 0
+                ReturnData = ReturnData + str(i)
+            elif i == 8:
+                i = 1
+                ReturnData = ReturnData + str(i)
+            elif i == 9:
+                i = 2
+                ReturnData = ReturnData + str(i)
+
+    return ReturnData
+
+
+userabc = input("passcode: ").split()
+print(string_to_data(userabc))
